52ea9c5d3bd0610059079d82f287acd2897705e6
Chibby124 <Dwysocki@manor.edu>
2021-10-20 09:46:11 -0400

    Adding task and task driver, missing toString and .equals functions.

---
+++ scanner-demo/src/main/java/com/revature/task.java
@@ -0,0 +1,14 @@
+package com.revature;
+
+public class task {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String testName) {
+		this.name = testName;
+	}
+	
+}
+
